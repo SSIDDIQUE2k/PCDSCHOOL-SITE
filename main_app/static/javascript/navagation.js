f0d457c4b132e962a2fe767d0b796b2e6e4ed30c
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-08-25 18:24:10 -0400

    Added css and Java script

--- main_app/static/javascript/navagation.js
+++ main_app/static/javascript/navagation.js
@@ -1,1 +1,13 @@
-const bar = document.querySelector()
+const bar = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+let menuOpen = false;
+
+bar.addEventListener('click', () => {
+    if (menuOpen == false) {
+        navLinks.style.display = 'block';
+        menuOpen = true;
+    } else { 
+        navLinks.style.display = 'none';
+        menuOpen = false;
+    }
+});
