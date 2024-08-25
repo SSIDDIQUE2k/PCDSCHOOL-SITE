51859918b2d4a2bdcfd4317c2b2cda4b5bc93c72
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-08-25 17:25:00 -0400

    created templates, images,

---
+++ main_app/views.py
@@ -0,0 +1,20 @@
+# main_app/views.py
+
+from django.shortcuts import render
+
+# Define the home view function
+def home(request):
+    """Home page view"""
+    return render(request, 'home.html')
+
+def about(request):
+    """About page view"""
+    return render(request, 'about.html')
+
+def services(request):
+    """Services page view"""
+    return render(request, 'services.html')
+
+def calendar(request):
+    """Calendar page view"""
+    return render(request, 'calendar.html')
