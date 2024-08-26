af06f186c668729d4a9b7f3f415efbe190bf118e
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-08-26 10:26:21 -0400

    fixed migrating and menu icon issue

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Teahers(models.Model):
+class Teacher(models.Model):
     name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
+    grade= models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    age = models.IntegerField()
+    subject = models.IntegerField()
 
     # new code below
     def __str__(self):
